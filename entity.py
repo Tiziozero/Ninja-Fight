bd7e552115776ee3e70467c2799b16ced3a12b45
Tiziozero <kleidiv123@gmail.com>
2023-12-02 20:45:02 +0000

    Before i mess up the code with image bank

--- entity.py
+++ entity.py
@@ -1,8 +1,46 @@
 import pygame, os, sys, time, math
 from debug import debug, print_debug
 
+
+class Images:
+    def __init__(self, path):
+        print("image for path", path)
+        self.images = {}
+        self.images_dirctory_path_relative_to_main_file = path
+        self.images = {}
+        self.image_rect = {}
+        self.image_dest_offset = 150
+        self.image_size = 200 # may vary
+        self.image_names = ['Attack1', 'Attack2', 'Death', 'Fall', 'Idle', 'Jump', 'Run', 'Take Hit']
+        self.image_indexes = ['Attack1', 'Attack2', 'Death', 'Fall', 'Idle', 'Jump', 'Run', 'Take Hit']
+        self.body_rect = pygame.Rect(0, 0, 0, 0)
+        self.image_dest_rect = {}
+        self.images_lef = {}
+        self.images_right = {}
+        self.image_index = 'Idle'
+        self.image_format = '.png'
+        self.blit_rect = pygame.Rect(0, 0, self.image_size, self.image_size)
+        self.body_rect = pygame.Rect(0, 0, 50, 50)
+        self.rect = pygame.Rect(0,0,0,0)
+        self.offset = 150
+        self.load_img(path)
+
+    def load_img(self, path):
+        for name in self.image_names:
+            path_to_image = path + name + self.image_format 
+
+            image_current = pygame.image.load(path_to_image).convert_alpha()
+
+            self.images[name] = [image_current, pygame.transform.flip(image_current, True, False)]
+            self.image_rect[name] = self.images[name][0].get_rect()
+            self.image_dest_rect[name] = [
+                                            [pygame.Rect(self.image_size * i, 0, self.image_size, self.image_size)  for i in range(self.image_rect[name].w // self.image_size)], # right
+                                            [pygame.Rect(self.image_rect[name].w - self.image_size - self.image_size * i, 0, self.image_size, self.image_size)  for i in range(self.image_rect[name].w // self.image_size)]  # left
+                                            ]
+
+
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, entity_id, img_dir_path, groups):
+    def __init__(self, entity_id, image_bank, img_dir_path, groups):
         super().__init__(groups)
         self.id = entity_id
         self.images = {}
@@ -17,14 +55,16 @@ class Entity(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         print(f"{str(self.id):_<10} rect w: {str(self.rect.w):_<5}, rect h: {str(self.rect.h):_<5}, rect x: {str(self.rect.x):_<5}, rect y: {str(self.rect.y):_<5}")
         self.dest_rect = pygame.Rect(0, 0, self.rect.w//8, self.rect.h)
-
+    """
     def draw(self, screen):
-        self.offset = 150
-        pygame.draw.rect(screen, (255, 255, 255), (self.rect.x + self.offset // 2, self.rect.y + self.offset // 2 - 3, self.rect.w // 8 - self.offset, self.rect.h - self.offset))
-        screen.blit(self.image, self.rect, self.dest_rect)
-
+        screen.blit(
+                    self.images[self.image_index][self.direction], # Imgae ( images, which image, what dirrection )
+                    self.blit_rect, # Draw rect ( where to draw )
+                    self.image_dest_rect[self.image_index][self.direction][int(self.dest_rect_index)] # Which tile ( destination rects, for which image, what direction )
+                )
+    """
     def update(self, screen, dt):
-        self.draw(screen)
+        # self.draw(screen)
         #self.dest_rect.x += self.dest_rect.w
         self.x_pos += dt
         self.dest_rect.x = self.x_pos
@@ -34,12 +74,14 @@ class Entity(pygame.sprite.Sprite):
             self.x_pos
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, entity_id, img_dir_path, groups, ground_group):
+    def __init__(self, entity_id, image_bank, img_dir_path, groups, ground_group):
         super().__init__(groups)
         # Player ID
         self.player_id = entity_id
         self.ground_group = ground_group
         print(len(self.ground_group))
+
+        self.image_bank = image_bank
 
         # Textures and Rect
         self.images_dirctory_path_relative_to_main_file = img_dir_path
@@ -123,78 +165,32 @@ class Player(pygame.sprite.Sprite):
                     if event.key == pygame.K_d:
                         self.horizontal_velocity -= self.velocity
 
-    """def updata_pos(self, dt, screen):
-        # Updata Position
-        self.vertical_velocity += 2500 * dt 
-
-
-        collision_with_ground = pygame.sprite.spritecollide(self, self.ground_group, dokill=False)
-
-        t = f"after calculation: {self.vertical_velocity}"
-        debug(t)
-        t = f"no of strites it collides with: {str(len(collision_with_ground)): <5} and self.body_rect.y {self.body_rect.y}"
-        debug(t)
-
-        for ground in collision_with_ground:
-            if self.body_rect.bottom > ground.rect.top:
-                if self.vertical_velocity >= 0:
-                    # self.body_rect.bottom = ground.rect.top
-
-                    self.y_position = ground.rect.top - self.body_rect.height
-                    self.vertical_velocity = 0
-
-                    
-                    self.jumping = False
-                    print(f" collision with vel >= 0, vel: {self.vertical_velocity}")
-
-                elif self.vertical_velocity < 0:
-                    self.body_rect.top = ground.rect.bottom
-                    self.y_position = ground.rect.bottom
-                    self.vertical_velocity = 0
-
-        t = f"after collision check: {self.vertical_velocity}"
-        debug(t)
-        if self.vertical_velocity == 0:
-            print("vvel is 0")
-        else:
-            print(f"vvel is not 0, {self.vertical_velocity}, {self.body_rect.y}")
-
-        if not self.attacking:
-            self.x_position += self.horizontal_velocity * dt 
-            self.y_position += self.vertical_velocity * dt 
-        self.body_rect.x = int(math.ceil(self.x_position))
-        self.body_rect.y = int(self.y_position)
-        t = f"after 1: {str(self.vertical_velocity): <25}, sefl.boddy_rext.: {self.body_rect.y}"
-        debug(t)"""
     def updata_pos(self, dt, screen):
         # Updata Position
         self.vertical_velocity += 2500 * dt 
 
+        # not using pygame spritecollide due to it giving problems like not getting collisions each timeits called maybe due to it being slow or any other reason
 
-        collision_with_ground = pygame.sprite.spritecollide(self, self.ground_group, dokill=False)
         for floor in self.ground_group:
             if self.rect.bottom >= floor.rect.top and self.rect.bottom <= floor.rect.bottom:
                 if self.rect.right >= floor.rect.left and self.rect.right <= floor.rect.right:
-                    debug("ye ye ye ye ye")
-                    print("ye ye ye ye ye")
-                    self.rect.bottom = floor.rect.top
-                    self.vertical_velocity = 0
-                    self.jumping = False
+                    if self.vertical_velocity >= 0:
+                        self.rect.bottom = floor.rect.top
+                        self.vertical_velocity = 0
+                        self.jumping = False
+            if self.rect.top <= floor.rect.bottom and self.rect.top >= floor.rect.top:
+                if self.rect.right >= floor.rect.left and self.rect.right <= floor.rect.right:
+                    if self.vertical_velocity <= 0:
+                        self.rect.top = floor.rect.bottom
+                        self.vertical_velocity = 0
 
-        t = f"after calculation: {self.vertical_velocity}"
-        debug(t)
-        t = f"no of strites it collides with: {str(len(collision_with_ground)): <5} and self.body_rect.y {self.body_rect.y}"
-        debug(t)
-        t = f"after collision check: {self.vertical_velocity}"
-        debug(t)
+
 
         if not self.attacking:
             self.x_position += self.horizontal_velocity * dt 
             self.y_position += self.vertical_velocity * dt 
         self.body_rect.x = int(math.ceil(self.x_position))
         self.body_rect.y = int(self.y_position)
-        t = f"after 1: {str(self.vertical_velocity): <25}, sefl.boddy_rext.: {self.body_rect.y}"
-        debug(t)
 
     def def_animation(self):
         # Update animation
@@ -202,18 +198,18 @@ class Player(pygame.sprite.Sprite):
 
             if self.jumping:
                 self.image_index= 'Jump'
-        if self.vertical_velocity > 0:
-            self.image_index = 'Fall'
-        elif self.vertical_velocity == 0:
-            self.image_index = 'Idle'
-            if self.horizontal_velocity > 0:
-                self.image_index = 'Run'
-                self.direction = 0
-            elif self.horizontal_velocity < 0:
-                self.image_index = 'Run'
-                self.direction = 1
-            else:
+            if self.vertical_velocity > 0:
+                self.image_index = 'Fall'
+            elif self.vertical_velocity == 0:
                 self.image_index = 'Idle'
+                if self.horizontal_velocity > 0:
+                    self.image_index = 'Run'
+                    self.direction = 0
+                elif self.horizontal_velocity < 0:
+                    self.image_index = 'Run'
+                    self.direction = 1
+                else:
+                    self.image_index = 'Idle'
 
 
     def draw(self, screen):
